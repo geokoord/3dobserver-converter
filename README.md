f6d63b5d31cfd4f0c955a4719f33f36f56377a00
Manuel Hart <manuel.hart@geokoord.com>
2021-04-16 07:30:40 +0200

    transform all data edit README.md

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # observer-file-transformer
-Transformation of 3-Dim Observer files to geojson
+
+Transformation of 3-Dim Observer files to GeoJson format
+
+```
+   ____            _                       _
+  / ___| ___  ___ | | _____   ___  _ __ __| |
+ | |  _ / _ \/ _ \| |/ / _ \ / _ \| '__/ _` |
+ | |_| |  __/ (_) |   < (_) | (_) | | | (_| |
+  \____|\___|\___/|_|\_\___/ \___/|_|  \__,_|
+3Dim-Observer to GeoJSON Converter | by Geokoord.com
+Version: 0.0.1
+```
+
+## Install
+
+run the following command to install dependencies
+
+```
+npm install
+```
+
+## Run
+
+```
+npm start
+```
--- index.js
+++ index.js
@@ -1,17 +1,30 @@
 const fsPromises = require("fs/promises");
+const fs = require("fs");
 const path = require("path");
 const lineReader = require("line-reader");
 const config = require("./package.json");
 
+const figlet = require("figlet");
+
+console.log(figlet.textSync("Geokoord"));
 console.log("\n3Dim-Observer to GeoJSON Converter | by Geokoord.com");
-console.log("Version: " + config.version + "\n");
+console.log(
+  "Version: " +
+    config.version +
+    "\n ------------------------------------------------------"
+);
+
+console.log("IN format: " + ".3dl");
+let outExt = ".geojson";
+console.log("OUT format: " + outExt);
+console.log("");
 
 let home = __dirname;
 
 (async () => {
   try {
     await readdir(home);
-    console.log("done.\n");
+    console.log("msg - done.\n");
   } catch (e) {
     console.log(e);
   }
@@ -36,15 +49,12 @@ async function readdir(dir) {
         let json = await threedl2geojson(file);
 
         //Write geojson file
-        await fsPromises.writeFile(p.name + ".geojson", JSON.stringify(json));
+        await fsPromises.writeFile(p.name + outExt, JSON.stringify(json));
 
-        console.log(
-          "Exported file " +
-            p.name +
-            ".geojson (" +
-            json.features.length +
-            " Features)"
-        );
+        console.log("Transform file " + file);
+        console.log("Contains " + json.features.length + " Features");
+        console.log("csr: " + "epsg:31467");
+        console.log("Write to file: " + p.name + outExt + "\n");
       }
     }
     resolve(true);
@@ -66,9 +76,9 @@ async function threedl2geojson(file) {
     //ReadFile
     let r = lineReader.eachLine(file, function (line, last) {
       if (line.startsWith("MPT")) {
-        //REM,ID,SortOrder,Name,Memo,Memo2,Meas.x,Meas.y,Meas.z,RealMeas.x,RealMeas.y,RealMeas.z,Ref.x,Ref.y,Ref.z,Offset.x,Offset.y,Offset.z,X_Check,Y_Check,Z_Check,MTol.x,MTol.y,MTol.z,PTol.x,PTol.y,PTol.z,Visible,Marked,PointType,Code,
-
         let lineSplit = line.split(",");
+
+        let decimalPlaces = 4;
 
         let name = lineSplit[3];
         let X = parseFloat((lineSplit[6] / 1000).toFixed(4));
@@ -82,6 +92,37 @@ async function threedl2geojson(file) {
             X: X,
             Y: Y,
             Z: Z,
+            REM: lineSplit[0],
+            ID: lineSplit[1],
+            SortOrder: lineSplit[2],
+            Name: lineSplit[3],
+            Memo: lineSplit[4],
+            Memo2: lineSplit[5],
+            "Meas.x": parseFloat((lineSplit[6] / 1000).toFixed(4)),
+            "Meas.y": parseFloat((lineSplit[7] / 1000).toFixed(4)),
+            "Meas.z": parseFloat((lineSplit[8] / 1000).toFixed(4)),
+            "RealMeas.x": parseFloat((lineSplit[9] / 1000).toFixed(4)),
+            "RealMeas.y": parseFloat((lineSplit[10] / 1000).toFixed(4)),
+            "RealMeas.z": parseFloat((lineSplit[11] / 1000).toFixed(4)),
+            "Ref.x": parseFloat((lineSplit[12] / 1000).toFixed(4)),
+            "Ref.y": parseFloat((lineSplit[13] / 1000).toFixed(4)),
+            "Ref.z": parseFloat((lineSplit[14] / 1000).toFixed(4)),
+            "Offset.x": parseFloat((lineSplit[15] / 1000).toFixed(4)),
+            "Offset.y": parseFloat((lineSplit[16] / 1000).toFixed(4)),
+            "Offset.z": parseFloat((lineSplit[17] / 1000).toFixed(4)),
+            X_Check: lineSplit[18],
+            Y_Check: lineSplit[19],
+            Z_Check: lineSplit[20],
+            "MTol.x": parseFloat((lineSplit[21] / 1000).toFixed(4)),
+            "MTol.y": parseFloat((lineSplit[22] / 1000).toFixed(4)),
+            "MTol.z": parseFloat((lineSplit[23] / 1000).toFixed(4)),
+            "PTol.x": parseFloat((lineSplit[24] / 1000).toFixed(4)),
+            "PTol.y": parseFloat((lineSplit[25] / 1000).toFixed(4)),
+            "PTol.z": parseFloat((lineSplit[26] / 1000).toFixed(4)),
+            Visible: lineSplit[27],
+            Marked: lineSplit[28],
+            PointType: lineSplit[29],
+            Code: lineSplit[30],
           },
           geometry: {
             type: "Point",
